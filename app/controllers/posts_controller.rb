3b72de059af37fb5e495b661e1b40c6fe4087fd8
Lindsay Mecher <lindsaymecher@gmail.com>
2020-05-13 16:20:47 -0500

    added edit and delete post functionality, updated about page with demos.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,7 @@ class PostsController < ApplicationController
       time: params[:time],
       venue_name: params[:venue_name],
       street_address: params[:street_address],
+      address_line_two: params[:address_line_two],
       city: params[:city],
       state: params[:state],
       zip: params[:zip],
@@ -30,6 +31,41 @@ class PostsController < ApplicationController
       user_honorific: params[:user_honorific],
       paid: params[:paid],
       user_id: params[:user_id]
+    )
+    render json: post
+  end
+
+  def destroy
+    p params
+    post = Post.find_by(id: params[:id])
+    post.destroy
+    render json: post 
+  end
+
+  def update
+    p params[:id].to_i
+    
+    post = Post.find_by(id: params[:id].to_i)
+    p post.user_id
+    p post.id
+    post.update(
+      performance_type: params[:performance_type],
+      voice_type: params[:voice_type],
+      date: params[:date],
+      time: params[:time],
+      venue_name: params[:venue_name],
+      street_address: params[:street_address],
+      address_line_two: params[:address_line_two],
+      city: params[:city],
+      state: params[:state],
+      zip: params[:zip],
+      repertoire: params[:repertoire],
+      notes: params[:notes],
+      contact_first_name: params[:contact_first_name],
+      contact_last_name: params[:contact_last_name],
+      contact_email: params[:contact_email],
+      user_honorific: params[:user_honorific],
+      paid: params[:paid]
     )
     render json: post
   end
