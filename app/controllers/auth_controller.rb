d7d97fc560c6f2cf6946fc7ea568ac90d3505efd
Lindsay Mecher <54283618+lindsayMecher@users.noreply.github.com>
2025-10-15 23:10:04 -0500

    (jwtfix) fix jwt configuration including removing secret. (#31)

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,10 +1,13 @@
 class AuthController < ApplicationController
+  def jwt_key
+    ENV['SESSION_SECRET']
+  end
+
   def create
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       payload = { user_id: user.id }
-      my_secret = '7d3b6a554654709543da3010ee931cd9c13baf6eae30186b1dcc4bcde95dc0211a75429c76cb14ba85c4e6d2ad7d6de28645830857fcd493b079b60d2397cbdb'
-      token = JWT.encode(payload, my_secret, 'HS256')
+      token = JWT.encode(payload, jwt_key, 'HS256')
       render json: { user: user, favorites: user.favorites, favorited_posts: user.favorited_posts, token: token }
       # render json: { user, include: [:favorite, :favorited_post], except: [:updated_at]} , token: token
     else
@@ -17,8 +20,7 @@ class AuthController < ApplicationController
     # then grab the user from db, then send the user to the front end
     auth = request.headers[:Authorization]
     token = auth.split(' ')[1]
-    my_secret = '7d3b6a554654709543da3010ee931cd9c13baf6eae30186b1dcc4bcde95dc0211a75429c76cb14ba85c4e6d2ad7d6de28645830857fcd493b079b60d2397cbdb'
-    decoded_token = JWT.decode token, my_secret, true, { algorithm: 'HS256' }
+    decoded_token = JWT.decode token, jwt_key, true, { algorithm: 'HS256' }
     user_id = decoded_token[0]['user_id']
     user = User.find_by(id: user_id)
     if user
