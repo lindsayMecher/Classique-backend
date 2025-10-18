1b684f0ea48ecf4c8c967e764c9f7c6fa4f68e0a
Lindsay Mecher <54283618+lindsayMecher@users.noreply.github.com>
2025-10-18 18:11:47 -0500

    (Bugfix/MS5) Resolve error with paid attr for New post. Add some error handling (#34)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,27 +10,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create(
-      performance_type: params[:performance_type],
-      voice_type: params[:voice_type],
-      date: params[:date],
-      time: params[:time],
-      venue_name: params[:venue_name],
-      street_address: params[:street_address],
-      address_line_two: params[:address_line_two],
-      city: params[:city],
-      state: params[:state],
-      zip: params[:zip],
-      repertoire: params[:repertoire],
-      notes: params[:notes],
-      contact_first_name: params[:contact_first_name],
-      contact_last_name: params[:contact_last_name],
-      contact_email: params[:contact_email],
-      user_honorific: params[:user_honorific],
-      paid: params[:paid],
-      user_id: params[:user_id]
-    )
-    render json: post
+    post = Post.new(post_params)
+    if post.save
+      render json: post
+    else
+      Rails.logger.error "Post creation failed: #{post.errors.full_messages}"
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -54,6 +40,6 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:performance_type, :voice_type, :date, :time, :venue_name, :street_address,
                                  :address_line_two, :city, :state, :zip, :repertoire, :notes, :contact_first_name,
-                                 :contact_last_name, :contact_email, :user_honorific, :paid)
+                                 :contact_last_name, :contact_email, :user_honorific, :paid, :user_id)
   end
 end
